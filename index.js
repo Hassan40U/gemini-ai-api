bfe27fb3a7055b77a1c50c34c0bb2d8bc8fa2f06
Hassan40U <164331446+Hassan40U@users.noreply.github.com>
2024-04-09 22:33:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv")
 dotenv.config()
 const readline = require("readline")
 // Access your API key as an environment variable (see "Set up your API key" above)
-const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+const genAI = new GoogleGenerativeAI(process.env.AIzaSyDfJLsDlbEQnzhEmL73AJc4mvJW53sVPFQ);
 
 const userInterface = readline.createInterface({
   input: process.stdin,
